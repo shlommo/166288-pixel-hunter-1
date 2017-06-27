393d57a133ad0e118c0b5b04fbe5c762adc19a14
szhakupbekov <szhakupbekov@wooppay.com>
2017-06-27 14:42:10 +0600

    исправлены ошибки линтера

--- js/main.js
+++ js/main.js
@@ -4,33 +4,30 @@ const renderTemplate = (num, arr) => {
   const container = document.querySelector(`.central`);
 
   container.innerHTML = arr[num].innerHTML;
-}
+};
 
 let altIsPressed = false;
 
 document.onkeydown = (event) => {
-  if(event.keyCode == 18) {
+  if (event.keyCode === 18) {
     altIsPressed = true;
   }
-}
+};
 
 document.onkeyup = (event) => {
-  if(event.keyCode == 18) {
+  if (event.keyCode === 18) {
     altIsPressed = false;
   }
-}
+};
 
 let tempNum = 0;
 
 document.addEventListener(`keydown`, (event) => {
-  if( altIsPressed && event.keyCode == 37 && tempNum > 0) {
+  if (altIsPressed && event.keyCode === 37 && tempNum > 0) {
     --tempNum;
     renderTemplate(tempNum, templatesArr);
-  } else if ( altIsPressed && event.keyCode == 39 && tempNum < templatesArr.length - 1 ){
+  } else if (altIsPressed && event.keyCode === 39 && tempNum < templatesArr.length - 1) {
     tempNum++;
     renderTemplate(tempNum, templatesArr);
   }
 });
-
-
-
